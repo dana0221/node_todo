b1b3d714b41bd63e26005dac4afd812a7cda5c9a
dana0221 <dana8478@naver.com>
2022-06-27 14:58:36 +0900

    input 기록 삭제

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const http = require('http')
 const fs = require('fs')
 
+// 서버생성
 const app = http.createServer(function(req, res){
     let url = '/views/index.html'
-    res.end(fs.readFileSync(__dirname + url))
+    res.end(fs.readFileSync(__dirname + url))   // index.html 불러와서 보여주기
 });
 
+// 포트번호 지정
 app.listen(3434)
